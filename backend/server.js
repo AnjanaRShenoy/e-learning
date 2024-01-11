f885a047cf9b87e95ee694dcda82dc2dff8a6be4
AnjanaRShenoy <shenoyanjana96@gmail.com>
2024-01-11 11:40:32 +0530

    vercel

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,15 @@ dotenv.config()
 import connectDB from "./config/db.js";
 import router from "./routes/studentRoutes.js";
 import adminRouter from "./routes/adminRoutes.js";
-import { notFound,errorHandler } from "./middleware/errorMiddleware.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 connectDB()
-const port= process.env.PORT || 5000
-const app= express()
+const port = process.env.PORT || 5000
+const app = express()
+app.use(cors({
+    origin: "*",
+    methods: ["POST", "GET"],
+    credentials: true
+}))
 app.use(express.json())
 
 app.use('/api/student', router)
@@ -16,6 +21,6 @@ app.use('/api/admin', adminRouter)
 app.use(notFound)
 app.use(errorHandler)
 
-app.get('/',(req,res)=> res.send("Server is ready"))
-app.listen(port,()=>console.log(`server started on port ${port}`))
+app.get('/', (req, res) => res.send("Server is ready"))
+app.listen(port, () => console.log(`server started on port ${port}`))
 
